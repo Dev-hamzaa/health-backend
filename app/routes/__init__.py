0dc329282d5f01aad1e2eb0742720a1e8a3aefb4
Dev-hamzaa <xzerefff@gmail.com>
2025-08-08 11:38:46 +0500

    working on medication

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,6 @@ from app.routes.patient_route import router as patient_router
 from app.routes.doctor_route import router as doctor_router
 from app.routes.appointment_route import router as appointment_router
 from app.routes.reviews_route import router as reviews_router
-# You can also import others like doctor_router, appointment_router here
 
 router = APIRouter()
 
